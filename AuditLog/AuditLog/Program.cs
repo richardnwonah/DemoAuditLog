cf4a4366d16f4a1b07aaf8f4477ea77ade3c8dae
richardnwonah <nwonahr@gmail.com>
2023-08-01 17:53:10 +0100

    Added cors functionality

--- AuditLog/AuditLog/Program.cs
+++ AuditLog/AuditLog/Program.cs
@@ -1,8 +1,21 @@
 using AuditLog.Services;
 
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.WithOrigins()
+                              .AllowAnyOrigin()
+                              .AllowAnyHeader()
+                               .AllowAnyMethod();
+                          });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
